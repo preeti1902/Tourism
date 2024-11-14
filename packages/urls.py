5d5ad2d1c7c3c73dc3a5f5275141d717aa659186
Rahul Mondal <rahulmondal779@gmail.com>
2024-11-14 23:00:02 +0530

    packgaes

--- packages/urls.py
+++ packages/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from packages.views import package_page,get_package
 
 urlpatterns = [
-    path('package/',package_page,name="package_page"),
+    path('packages/',package_page,name="package_page"),
     path('<slug>/',get_package,name='get_package')
 ]
